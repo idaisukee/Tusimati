ee6a5c608f6dc749a5c172d30a298a180db80b9c
Daisuke Inoue <hibariyashiki@gmail.com>
2017-11-20 19:26:28 +0900

    color order

--- Tusimati.el
+++ Tusimati.el
@@ -9,7 +9,7 @@
     `(whitespace-tab
          ((t (
                  :inherit whitespace-space
-                 :foreground "#75715E"))))
+                 :foreground "#75715"))))
     `(dired-k-directory
          ((t (
                  :foreground ,monokai-blue)))))
@@ -18,13 +18,13 @@
     `(dired-k-size-colors
          `(
               (1024 . ,monokai-cyan)
-              (2048 . ,monokai-magenta)
+              (2048 . ,monokai-blue)
               (3072 . ,monokai-blue)
               
-              (5120 . ,monokai-violet)
-              (10240 . ,monokai-green)
-              (20480 . ,monokai-yellow)
-              (40960 . ,monokai-yellow)
+              (5120 . ,monokai-green)
+              (10240 . ,monokai-yellow)
+              (20480 . ,monokai-violet)
+              (40960 . ,monokai-magenta)
               (102400 . ,monokai-orange)
               (262144 . ,monokai-red)
               (524288 . ,monokai-red))))
@@ -34,13 +34,13 @@
          `(
               (0 . ,monokai-red)
               (60 . ,monokai-orange)
-              (3600 . ,monokai-yellow)
+              (3600 . ,monokai-magenta)
               
-              (86400 . ,monokai-green)
-              (604800 . ,monokai-violet)
+              (86400 . ,monokai-violet)
+              (604800 . ,monokai-yellow)
               (2419200 . ,monokai-blue)
               
-              (15724800 . ,monokai-magenta)
+              (15724800 . ,monokai-blue)
               (31449600 . ,monokai-cyan)
               (62899200 . ,monokai-cyan))))
 
